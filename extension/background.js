96e32ab59bacc2bc640f5cba91e20d6a16fd73d6
kapustin <kapustin4613@gmail.com>
2025-11-02 08:54:13 +0900

    update

--- extension/background.js
+++
@@ -1,5 +0,0 @@
-// Service Worker는 설치 시 실행됩니다.
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Serp AI Score Extension installed.");
-});
-
--- extension/cp.sh
+++
@@ -1,3 +0,0 @@
-### 구글 익스텐션 프로그램 업로드 할 때 wsl2를 인식못해서 그냥 windows 에다가 복사해서 사용
-mkdir -p /mnt/c/dev/serp-ai-score-extension
-cp -r ~/ai-smell/extension/* /mnt/c/dev/serp-ai-score-extension/

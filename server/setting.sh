2a9ee588e07a2789fe07a4add0c2f56f04b73d34
kapustin <kapustin4613@gmail.com>
2025-11-03 20:22:06 +0900

    asdf

--- server/setting.sh
+++ server/setting.sh
@@ -5,12 +5,7 @@ mkdir -p ~/models/my_gguf_model
 mv /model-Q5_K_M.gguf ~/models/my_gguf_model/
 
 cat > ~/models/my_gguf_model/Modelfile <<'EOF'
-FROM ./model.gguf
-# 선택: 문맥창, 로프스케일 등
-# PARAMETER num_ctx 4096
-# PARAMETER rope_frequency_base 1e6
-# 선택: 프롬프트 템플릿 지정
-# TEMPLATE "{{ .System }}\n{{ .Prompt }}"
+FROM ./model-Q5_K_M.gguf
 EOF
 
 ollama create my_gguf_model -f ~/models/my_gguf_model/Modelfile
